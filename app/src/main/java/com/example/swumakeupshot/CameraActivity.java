64b03e5b7e90c467da571eae535e745f6f8c2d6c
hongchopper <seonmihong69@gmail.com>
2022-10-04 18:40:57 +0900

    10/4 Camera

--- app/src/main/java/com/example/swumakeupshot/CameraActivity.java
+++ app/src/main/java/com/example/swumakeupshot/CameraActivity.java
@@ -4,221 +4,295 @@ import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.graphics.drawable.BitmapDrawable;
+import android.media.ExifInterface;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.StrictMode;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-import java.util.List;
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.content.FileProvider;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class CameraActivity extends AppCompatActivity implements View.OnClickListener{
-    /*ImageView imageView;
-    String mCurrentPhotoPath;
-    Button camerabtn;
-    Button selectbtn;
+public class CameraActivity extends AppCompatActivity {
+    private static final String TAG = "CameraActivity";
+
+    public static final int REQUEST_TAKE_PHOTO = 10;
+    public static final int REQUEST_PERMISSION = 11;
+
+    private Button btnCamera, btnSave;
+    private ImageView ivCapture ;
+    private String mCurrentPhotoPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_crop);
+        setContentView(R.layout.activity_camera);
 
-        imageView = (ImageView) findViewById(R.id.quick_start_cropped_image);
-        camerabtn = (Button) findViewById(R.id.onCameraClick);
-        selectbtn = (Button) findViewById(R.id.onSelectImageClick);
-        camerabtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-                    File photoFile = null;
+        checkPermission(); //권한체크
 
-                    File tempDir = getCacheDir();
+        ivCapture = findViewById(R.id.quick_start_cropped_image); //ImageView 선언
+        btnCamera = findViewById(R.id.onCameraClick); //Button 선언
+        btnSave = findViewById(R.id.onSelectImageClick); //Button 선언
 
-                    String timeStamp = new SimpleDateFormat("yyyyMdd_HHmmss").format(new Date());
-                    String imageFileName = "Capture_" + timeStamp + "_";
+        loadImgArr();
 
-                    try {
-                        File tempImage = File.createTempFile(imageFileName, ".jpg", tempDir);
-                        mCurrentPhotoPath = tempImage.getAbsolutePath();
-                        photoFile = tempImage;
+        //촬영
+        btnCamera.setOnClickListener(v -> captureCamera());
 
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
+        //저장
+        btnSave.setOnClickListener(v -> {
 
-                    if (photoFile != null) {
-                        Uri photoURI = FileProvider.getUriForFile(CameraActivity.this, getPackageName() + ".fileprovider", photoFile);
-                        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
-                        startActivityForResult(takePictureIntent, 1);
-                    }
+            try {
+                BitmapDrawable drawable = (BitmapDrawable) ivCapture.getDrawable();
+                Bitmap bitmap = drawable.getBitmap();
+
+                //찍은 사진이 없으면
+                if (bitmap == null) {
+                    Toast.makeText(this, "저장할 사진이 없습니다.", Toast.LENGTH_SHORT).show();
+                } else {
+                    //저장
+                    saveImg();
+                    mCurrentPhotoPath = ""; //initialize
+                }
+
+            } catch (Exception e) {
+                Log.w(TAG, "SAVE ERROR!", e);
+            }
+        });
+    }
+
+    private void captureCamera() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        // 인텐트를 처리 할 카메라 액티비티가 있는지 확인
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+
+            // 촬영한 사진을 저장할 파일 생성
+            File photoFile = null;
+
+            try {
+                //임시로 사용할 파일이므로 경로는 캐시폴더로
+                File tempDir = getCacheDir();
+
+                //임시촬영파일 세팅
+                String timeStamp = new SimpleDateFormat("yyyyMMdd").format(new Date());
+                String imageFileName = "Capture_" + timeStamp + "_"; //ex) Capture_20201206_
+
+                File tempImage = File.createTempFile(
+                        imageFileName,  /* 파일이름 */
+                        ".jpg",         /* 파일형식 */
+                        tempDir      /* 경로 */
+                );
+
+                // ACTION_VIEW 인텐트를 사용할 경로 (임시파일의 경로)
+                mCurrentPhotoPath = tempImage.getAbsolutePath();
+
+                photoFile = tempImage;
+
+            } catch (IOException e) {
+                //에러 로그는 이렇게 관리하는 편이 좋다.
+                Log.w(TAG, "파일 생성 에러!", e);
+            }
+
+            //파일이 정상적으로 생성되었다면 계속 진행
+            if (photoFile != null) {
+                //Uri 가져오기
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        getPackageName() + ".fileprovider",
+                        photoFile);
+                //인텐트에 Uri담기
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+
+                //인텐트 실행
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
+    }
+
+    //이미지저장 메소드
+    private void saveImg() {
+
+        try {
+            //저장할 파일 경로
+            File storageDir = new File(getFilesDir() + "/capture");
+            if (!storageDir.exists()) //폴더가 없으면 생성.
+                storageDir.mkdirs();
+
+            String filename = "캡쳐파일" + ".jpg";
+
+            // 기존에 있다면 삭제
+            File file = new File(storageDir, filename);
+            boolean deleted = file.delete();
+            Log.w(TAG, "Delete Dup Check : " + deleted);
+            FileOutputStream output = null;
+
+            try {
+                output = new FileOutputStream(file);
+                BitmapDrawable drawable = (BitmapDrawable) ivCapture.getDrawable();
+                Bitmap bitmap = drawable.getBitmap();
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 70, output); //해상도에 맞추어 Compress
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    assert output != null;
+                    output.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
             }
 
-        });
+            Log.e(TAG, "Captured Saved");
+            Toast.makeText(this, "Capture Saved ", Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            Log.w(TAG, "Capture Saving Error!", e);
+            Toast.makeText(this, "Save failed", Toast.LENGTH_SHORT).show();
 
-    }
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable @org.jetbrains.annotations.Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == 1) {
-            if (requestCode == RESULT_OK) {
-                File file = new File(mCurrentPhotoPath);
-                //CropImage.activity(Uri.fromFile(file)).start(CameraActivity.this);
-            }
         }
-    }*/
-    ImageView imageView;
-    Button btnCamera;
-    final static int TAKE_PICTURE = 1;
-    final static int CROP_PICTURE = 2;
-    private Uri pictureUri;
+    }
+
+    private void loadImgArr() {
+        try {
+
+            File storageDir = new File(getFilesDir() + "/capture");
+            String filename = "캡쳐파일" + ".jpg";
+
+            File file = new File(storageDir, filename);
+            Bitmap bitmap = BitmapFactory.decodeStream(new FileInputStream(file));
+            ivCapture.setImageBitmap(bitmap);
+
+        } catch (Exception e) {
+            Log.w(TAG, "Capture loading Error!", e);
+            Toast.makeText(this, "load failed", Toast.LENGTH_SHORT).show();
+        }
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_crop);
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+        try {
+            //after capture
+            switch (requestCode) {
+                case REQUEST_TAKE_PHOTO: {
+                    if (resultCode == RESULT_OK) {
 
-        imageView = (ImageView)findViewById(R.id.quick_start_cropped_image);
-        btnCamera = (Button)findViewById(R.id.onCameraClick);
+                        File file = new File(mCurrentPhotoPath);
+                        Bitmap bitmap = MediaStore.Images.Media
+                                .getBitmap(getContentResolver(), Uri.fromFile(file));
 
-        btnCamera.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                if(takePictureIntent.resolveActivity(getPackageManager())!=null){
-                    File photoFile = null;
+                        if (bitmap != null) {
+                            ExifInterface ei = new ExifInterface(mCurrentPhotoPath);
+                            int orientation = ei.getAttributeInt(ExifInterface.TAG_ORIENTATION,
+                                    ExifInterface.ORIENTATION_UNDEFINED);
 
-                    File tempDir = getCacheDir();
+//                            //사진해상도가 너무 높으면 비트맵으로 로딩
+//                            BitmapFactory.Options options = new BitmapFactory.Options();
+//                            options.inSampleSize = 8; //8분의 1크기로 비트맵 객체 생성
+//                            Bitmap bitmap = BitmapFactory.decodeFile(file.getAbsolutePath(), options);
 
-                    String timeStamp = new SimpleDateFormat("yyyyMdd_HHmmss").format(new Date());
-                    String imageFileName = "Capture_" + timeStamp+ "_";
+                            Bitmap rotatedBitmap = null;
+                            switch (orientation) {
 
-                    try{
-                        File tempImage = File.createTempFile(imageFileName, ".jpg", tempDir);
-                        //mCurrentPhotoPath = tempImage.getAbsolutePath();
-                        photoFile = tempImage;
+                                case ExifInterface.ORIENTATION_ROTATE_90:
+                                    rotatedBitmap = rotateImage(bitmap, 90);
+                                    break;
 
-                    } catch (IOException e){
-                        e.printStackTrace();
+                                case ExifInterface.ORIENTATION_ROTATE_180:
+                                    rotatedBitmap = rotateImage(bitmap, 180);
+                                    break;
+
+                                case ExifInterface.ORIENTATION_ROTATE_270:
+                                    rotatedBitmap = rotateImage(bitmap, 270);
+                                    break;
+
+                                case ExifInterface.ORIENTATION_NORMAL:
+                                default:
+                                    rotatedBitmap = bitmap;
+                            }
+
+                            //Rotate한 bitmap을 ImageView에 저장
+                            ivCapture.setImageBitmap(rotatedBitmap);
+
+                        }
                     }
-
-                    if(photoFile!=null){
-                        Uri photoURI = FileProvider.getUriForFile(CameraActivity.this, getPackageName()+".fileprovider", photoFile);
-                        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
-                        startActivityForResult(takePictureIntent, 1);
-                    }
-                }
-            }
-        });
-
-        //Uri exposure 무시
-        StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
-        StrictMode.setVmPolicy(builder.build());
-
-        //권한 요청
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){ //안드로이드 버전확인
-            //권한 허용이 됐는지 확인
-            if(checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
-
-            }
-            else { //권한 허용 요청
-                ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
-            }
-        }
-    }
-
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-
-        if(grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED && grantResults[2] == PackageManager.PERMISSION_GRANTED){
-
-        }
-    }
-
-
-    @Override
-    public void onClick(View view) {
-        switch (view.getId())
-        {
-            // 버튼 눌렀을 때, 카메라에서 이미지 가져오기
-            case R.id.onCameraClick:
-                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-
-                // 임시로 사용할 파일의 경로를 생성
-                String url = "tmp_" + String.valueOf(System.currentTimeMillis()) + ".jpg";
-                pictureUri = Uri.fromFile(new File(Environment.getExternalStorageDirectory(), url));
-
-                cameraIntent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, pictureUri);
-                cameraIntent.putExtra("return-data", true);
-                startActivityForResult(cameraIntent, CROP_PICTURE);
-                break;
-        }
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        switch (requestCode) {
-            //크롭된 이미지 가져와서 이미지뷰에 보여주기
-            case TAKE_PICTURE:
-                if (resultCode == RESULT_OK && data.hasExtra("data")) { //데이터를 가지고 있는지 확인
-                    final Bundle extras = data.getExtras();
-
-                    if(extras != null)
-                    {
-                        //File file = new File(mCurrentPhotoPath);
-                        //CropImage.activity(Uri.fromFile(file).start(CameraActivity.this);
-                        
-                        //비트맵 방식
-                        //Bitmap photo = extras.getParcelable("data"); //크롭한 이미지 가져오기
-                        //imageView.setImageBitmap(photo); //이미지뷰에 넣기
-                    }
-
-                    // 임시 파일 삭제
-                    File f = new File(pictureUri.getPath());
-                    if(f.exists())
-                        f.delete();
-
                     break;
                 }
-                break;
+            }
 
-            // 이미지 크롭
-            case CROP_PICTURE: {
-                // 이미지를 가져온 이후의 리사이즈할 이미지 크기를 결정합니다.
-                // 이후에 이미지 크롭 어플리케이션을 호출하게 됩니다.
+        } catch (Exception e) {
+            Log.w(TAG, "onActivityResult Error !", e);
+        }
+    }
 
-                Intent intent = new Intent("com.android.camera.action.CROP");
-                intent.setDataAndType(pictureUri, "image/*");
+    //카메라에 맞게 이미지 로테이션
+    public static Bitmap rotateImage(Bitmap source, float angle) {
+        Matrix matrix = new Matrix();
+        matrix.postRotate(angle);
+        return Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(), matrix, true);
+    }
 
-                intent.putExtra("outputX", 200); //크롭한 이미지 x축 크기
-                intent.putExtra("outputY", 200); //크롭한 이미지 y축 크기
-                intent.putExtra("aspectX", 1); //크롭 박스의 x축 비율
-                intent.putExtra("aspectY", 1); //크롭 박스의 y축 비율
-                intent.putExtra("scale", true);
-                intent.putExtra("return-data", true);
-                startActivityForResult(intent, TAKE_PICTURE);
+    @Override
+    public void onResume() {
+        super.onResume();
+        checkPermission(); //권한체크
+    }
 
-                break;
+    //권한 확인
+    public void checkPermission() {
+        int permissionCamera = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
+        int permissionRead = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE);
+        int permissionWrite = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        //권한이 없으면 권한 요청
+        if (permissionCamera != PackageManager.PERMISSION_GRANTED
+                || permissionRead != PackageManager.PERMISSION_GRANTED
+                || permissionWrite != PackageManager.PERMISSION_GRANTED) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)) {
+                Toast.makeText(this, "이 앱을 실행하기 위해 권한이 필요합니다.", Toast.LENGTH_SHORT).show();
+            }
+
+            ActivityCompat.requestPermissions(this, new String[]{
+                    Manifest.permission.CAMERA, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_PERMISSION);
+
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case REQUEST_PERMISSION: {
+                // 권한이 취소되면 result 배열은 비어있다.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    Toast.makeText(this, "권한 확인", Toast.LENGTH_LONG).show();
+
+                } else {
+                    Toast.makeText(this, "권한 없음", Toast.LENGTH_LONG).show();
+                    finish(); //권한이 없으면 앱 종료
+                }
             }
         }
     }
