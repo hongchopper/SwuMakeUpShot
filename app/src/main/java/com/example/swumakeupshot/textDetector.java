4e38bdb7d0b4df6565bfa3aaf6399aaaa55e3410
Yewon Kim <victory2588@naver.com>
2022-10-04 21:54:43 +0900

    사진찍기까지만 일단 구현

--- app/src/main/java/com/example/swumakeupshot/textDetector.java
+++ app/src/main/java/com/example/swumakeupshot/textDetector.java
@@ -1,5 +1,7 @@
 package com.example.swumakeupshot;
 
+import static android.content.ContentValues.TAG;
+
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -11,9 +13,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
 import com.bumptech.glide.Glide;
 import com.google.android.gms.tasks.OnFailureListener;
@@ -25,19 +29,26 @@ import com.google.mlkit.vision.text.TextRecognition;
 import com.google.mlkit.vision.text.TextRecognizer;
 import com.google.mlkit.vision.text.korean.KoreanTextRecognizerOptions;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class textDetector extends AppCompatActivity {
     static final int REQUEST_CODE = 2;
+    public static final int REQUEST_TAKE_PHOTO = 10;
 
     ImageView imageView;    // 갤러리에서 가져온 이미지를 보여줄 뷰
     Uri uri;                // 갤러리에서 가져온 이미지에 대한 Uri
     Bitmap bitmap;          // 갤러리에서 가져온 이미지를 담을 비트맵
     InputImage image;       // ML 모델이 인식할 인풋 이미지
     TextView text_info;     // ML 모델이 인식한 텍스트를 보여줄 뷰
-    Button btn_get_image, btn_detection_image;  // 이미지 가져오기 버튼, 이미지 인식 버튼
+    Button btn_get_image, btn_detection_image,btnCamera;  // 이미지 가져오기 버튼, 이미지 인식 버튼
     TextRecognizer recognizer;    //텍스트 인식에 사용될 모델
+    private String mCurrentPhotoPath;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,6 +57,11 @@ public class textDetector extends AppCompatActivity {
         imageView = findViewById(R.id.quick_start_cropped_image);
         text_info = findViewById(R.id.text_info);
         recognizer = TextRecognition.getClient(new KoreanTextRecognizerOptions.Builder().build());    //텍스트 인식에 사용될 모델
+
+
+        // CAMERA CLICK 버튼
+        btnCamera = findViewById(R.id.onCameraClick);
+        btnCamera.setOnClickListener(v -> captureCamera());
 
         // GET IMAGE 버튼
         btn_get_image = findViewById(R.id.getPic);
@@ -66,8 +82,54 @@ public class textDetector extends AppCompatActivity {
                 TextRecognition(recognizer);
             }
         });
+    }
 
+    private void captureCamera() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 
+        // 인텐트를 처리 할 카메라 액티비티가 있는지 확인
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+
+            // 촬영한 사진을 저장할 파일 생성
+            File photoFile = null;
+
+            try {
+                //임시로 사용할 파일이므로 경로는 캐시폴더로
+                File tempDir = getCacheDir();
+
+                //임시촬영파일 세팅
+                String timeStamp = new SimpleDateFormat("yyyyMMdd").format(new Date());
+                String imageFileName = "Capture_" + timeStamp + "_"; //ex) Capture_20201206_
+
+                File tempImage = File.createTempFile(
+                        imageFileName,  //파일이름
+                        ".jpg",    // 파일형식
+                        tempDir      //경로
+                );
+
+                // ACTION_VIEW 인텐트를 사용할 경로 (임시파일의 경로)
+                mCurrentPhotoPath = tempImage.getAbsolutePath();
+
+                photoFile = tempImage;
+
+            } catch (IOException e) {
+                //에러 로그는 이렇게 관리하는 편이 좋다.
+                Log.w(TAG, "파일 생성 에러!", e);
+            }
+
+            //파일이 정상적으로 생성되었다면 계속 진행
+            if (photoFile != null) {
+                //Uri 가져오기
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        getPackageName() + ".fileprovider",
+                        photoFile);
+                //인텐트에 Uri담기
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+
+                //인텐트 실행
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
     }
 
     @Override
@@ -115,5 +177,7 @@ public class textDetector extends AppCompatActivity {
                                 Log.e("텍스트 인식", "실패: " + e.getMessage());
                             }
                         });
+
     }
+
 }
